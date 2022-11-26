e8ad31251a8fce2e650388b0ef97313b73b34e9f
unknown <sarahidms@gmail.com>
2022-11-26 19:44:16 -0300

    landing page without css

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import style from './LandingPage.module.css'
+import image from "./img/dog.jpg"
 
 const LandingPage = () => {
     return (
-        <div>
+        <div className={`${style.body}`}>
+            <img className={`${style.img}`} src={image} alt='dog'/>
             <Link to='/home'>
-                <button>Home</button>
+                <button className={`${style.home}`}>Home</button>
             </Link>
         </div>
     ) 
